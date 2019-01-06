5e7fdc2445f0eda119164ae2127804d87500d593
Spencer Caretta <spencer.caretta@gmail.com>
2019-01-06 00:12:51 -0600

    minor fix

--- models/burger.js
+++ models/burger.js
@@ -2,17 +2,17 @@ var orm = require("../config/orm.js");
 
 var burger = {
     all: function(cb) {
-        orm.all("burgers", function(res) {
+        orm.selectAll("burgers", function(res) {
             cb(res);
         });
     },
     create: function(cols, vals, cb) {
-        orm.create("burgers", cols, vals, function(res){
+        orm.insertOne("burgers", cols, vals, function(res){
             cb(res);
         })
     },
     update: function(condition, cb) {
-        orm.update("burgers", condition, function(res) {
+        orm.updateOne("burgers", condition, function(res) {
             cb(res);
         })
     }
